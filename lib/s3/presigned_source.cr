5b66e955e973dcffe470cba08137e6871ff221b5
Michal Szmyd <michal.szmyd@icloud.com>
2024-03-13 18:47:38 +0100

    Inverse of expired statement for better readbility

--- lib/s3/presigned_source.cr
+++ lib/s3/presigned_source.cr
@@ -10,7 +10,7 @@ module S3
     def expired?
       return true if (expires.nil? || created_at.nil?)
 
-      created_at.not_nil!.to_unix + expires.not_nil! <= Time.utc.to_unix
+      Time.utc.to_unix > created_at.not_nil!.to_unix + expires.not_nil!
     end
   end
 end
