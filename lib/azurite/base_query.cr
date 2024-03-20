ae130595f2124ceb23d701194d17eb4b951feb79
Michal Szmyd <michal.szmyd@icloud.com>
2024-03-20 20:08:02 +0100

    Added azurite macros

--- lib/azurite/base_query.cr
+++ lib/azurite/base_query.cr
@@ -1,22 +1,48 @@
+require "./relations"
 
 module Azurite
   class BaseQuery(T)
+    macro table_name(name)
+      def table_name
+        {{name.stringify}}
+      end
+    end
+
+    include Relations
     include Enumerable(T)
 
     class RecordNotFound < Exception
     end
 
     getter select_clause : String = "*"
-    getter from_clause : String
+    getter joins_conditions : Array(String)
 
     @limit_value : Int32?
+    @offset_value : Int32?
+    @order_value : String?
+    @where_params : {args: Array(WhereConditionType), where_clause: String}?
 
     def initialize(
-      @select_clause : String?,
-      @from_clause : String,
+      @select_clause : String?
     )
       @where_conditions = [] of WhereCondition
+      @joins_conditions = [] of String
       @limit_value = nil
+      @offset_value = nil
+      @order_value = nil
+      @where_params = nil
+    end
+
+    def select(raw)
+      @select_clause = raw
+
+      self
+    end
+
+    def joins(raw)
+      @joins_conditions.push(raw)
+
+      self
     end
 
     def find(id)
@@ -27,12 +53,39 @@ module Azurite
       record
     end
 
+    def select_count
+      args = where_params[:args]
+      where_clause = where_params[:where_clause]
+
+      count_sql = "SELECT count(*) FROM #{table_name} #{joins_clause} #{where_clause}"
+
+      count_value = AppDatabase.scalar(count_sql, args: args)
+
+      if count_value.is_a?(Int32)
+        count_value
+      else
+        0
+      end
+    end
+
     def first?
       results[0]?
     end
 
     def limit(value : Int32)
       @limit_value = value
+
+      self
+    end
+
+    def offset(value : Int32)
+      @offset_value = value
+
+      self
+    end
+
+    def order(value : String)
+      @order_value = value
 
       self
     end
@@ -81,36 +134,59 @@ module Azurite
       self
     end
 
-    def each(&block : T -> _)
-      results.each(&block)
+    def each(&block : T ->)
+      results do |element|
+        yield element
+      end
     end
 
     def results
       params = execute_params
 
-      AppDatabase.query_all(params[:sql], args: params[:args], as: T)
+      AppDatabase.query_each(params[:sql], args: params[:args]) do |element|
+        yield element.read T
+      end
     end
 
     def execute_params
-      args = [] of WhereConditionType
-      index_of_where_parameter = 0
-
-      where_condition = @where_conditions.map_with_index do |where, index|
-        if where.value.nil?
-          where.to_query
-        else
-          args.push(where.value)
-          where.to_query(index_of_where_parameter += 1)
-        end
-      end.join(" AND ")
-
-      where_clause = where_condition.blank? ? "" : "WHERE #{where_condition}"
       limit = @limit_value ? "LIMIT #{@limit_value}" : ""
+      offset = @offset_value ? "OFFSET #{@offset_value}" : ""
+      order = @order_value ? "ORDER BY #{@order_value}" : ""
+
+      args = where_params[:args]
+      where_clause = where_params[:where_clause]
 
       {
-        sql:"SELECT #{select_clause} FROM #{from_clause} #{where_clause} #{limit}",
+        sql: "SELECT #{select_clause} FROM #{table_name} #{joins_clause} #{where_clause} #{order} #{limit} #{offset}",
         args: args
       }
+    end
+
+    def joins_clause
+      @joins_conditions.empty? ? "" : @joins_conditions.join(" ")
+    end
+
+    def where_params
+      @where_params ||= begin
+        args = [] of WhereConditionType
+        index_of_where_parameter = 0
+
+        where_condition = @where_conditions.map_with_index do |where, index|
+          if where.value.nil?
+            where.to_query
+          else
+            args.push(where.value)
+            where.to_query(index_of_where_parameter += 1)
+          end
+        end.join(" AND ")
+
+        where_clause = where_condition.blank? ? "" : "WHERE #{where_condition}"
+
+        {
+          args: args,
+          where_clause: where_clause
+        }
+      end
     end
   end
 end
---
+++ lib/azurite/relations.cr
@@ -0,0 +1,25 @@
+module Azurite
+  module Relations
+    macro has_many(name, key = "id")
+      def joins_{{name}}
+        joins("INNER JOIN {{name.id}} ON {{name.id}}.{{key.id}} = #{table_name}.id")
+      end
+
+      def left_joins_{{name}}
+        joins("LEFT JOIN {{name.id}} ON {{name.id}}.{{key.id}} = #{table_name}.id")
+      end
+    end
+
+    macro belongs_to(name, foreign_key)
+      {{join_table_name = "#{name.id}s"}}
+
+      def joins_{{name}}
+        joins("INNER JOIN #{{{join_table_name}}} ON #{{{join_table_name}}}.id = #{table_name}.{{foreign_key.id}}")
+      end
+
+      def left_joins_{{name}}
+        joins("LEFT JOIN #{{{join_table_name}}} ON #{{{join_table_name}}}.id = #{table_name}.{{foreign_key.id}}")
+      end
+    end
+  end
+end
